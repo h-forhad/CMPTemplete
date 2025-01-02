30127ff23abc5c044684bbfd53989d342d2ac50e
WHKQ63 <mohammadforhad.hossain@motorolasolutions.com>
2025-01-02 15:15:45 +0800

    Prepare for Release 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,7 +122,7 @@ object App {
     object Versions {
         val major: Int = 0
         val minor: Int = 0
-        val patch: Int = 2
+        val patch: Int = 3
 
         val suffix: String? = null
 
